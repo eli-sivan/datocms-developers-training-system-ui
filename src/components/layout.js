7ef3407bd806dee9352b194897f3cc0198676afe
esivantemp <eli.sivan@naturalint.com>
2019-11-01 17:09:09 +0200

    add new model - question

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@ import { HelmetDatoCms } from 'gatsby-source-datocms'
 import '../styles/index.sass'
 
 const TemplateWrapper = ({ children }) => (
-  <StaticQuery query={graphql`
+    <StaticQuery query={graphql`
     query LayoutQuery
     {
       datoCmsSite {
@@ -39,66 +39,67 @@ const TemplateWrapper = ({ children }) => (
       }
     }
   `}
-  render={data => (
-    <div className="container">
-      <HelmetDatoCms
-        favicon={data.datoCmsSite.faviconMetaTags}
-        seo={data.datoCmsHome.seoMetaTags}
-      />
-      <div className="container__sidebar">
-        <div className="sidebar">
-          <h6 className="sidebar__title">
-            <Link to="/">{data.datoCmsSite.globalSeo.siteName}</Link>
-          </h6>
-          <div
-            className="sidebar__intro"
-            dangerouslySetInnerHTML={{
-              __html: data.datoCmsHome.introTextNode.childMarkdownRemark.html,
-            }}
-          />
-          <ul className="sidebar__menu">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-          <p className="sidebar__social">
-            {data.allDatoCmsSocialProfile.edges.map(({ node: profile }) => (
-              <a
-                key={profile.profileType}
-                href={profile.url}
-                target="blank"
-                className={`social social--${profile.profileType.toLowerCase()}`}
-              > </a>
-            ))}
-          </p>
-          <div className="sidebar__copyright">{data.datoCmsHome.copyright}</div>
-        </div>
-      </div>
-      <div className="container__body">
-        <a href="https://www.google.com">Google</a>
-
-        <div className="container__mobile-header">
-          <div className="mobile-header">
-            <div className="mobile-header__menu">
-              <Link to="#" data-js="toggleSidebar" />
-            </div>
-            <div className="mobile-header__logo">
-              <Link to="/">{data.datoCmsSite.globalSeo.siteName}</Link>
-            </div>
-          </div>
-        </div>
-        {children}
-      </div>
-    </div>
-    )}
-  />
+                 render={data => (
+                     <div className="container">
+                         <HelmetDatoCms
+                             favicon={data.datoCmsSite.faviconMetaTags}
+                             seo={data.datoCmsHome.seoMetaTags}
+                         />
+                         <div className="container__sidebar">
+                             <div className="sidebar">
+                                 <h6 className="sidebar__title">
+                                     <Link to="/">{data.datoCmsSite.globalSeo.siteName}</Link>
+                                 </h6>
+                                 <div
+                                     className="sidebar__intro"
+                                     dangerouslySetInnerHTML={{
+                                         __html: data.datoCmsHome.introTextNode.childMarkdownRemark.html,
+                                     }}
+                                 />
+                                 <ul className="sidebar__menu">
+                                     <li>
+                                         <Link to="/">Home</Link>
+                                     </li>
+                                     <li>
+                                         <Link to="/about">About</Link>
+                                     </li>
+                                     <li>
+                                         <Link to="/poc">POC</Link>
+                                     </li>
+                                 </ul>
+                                 <p className="sidebar__social">
+                                     {data.allDatoCmsSocialProfile.edges.map(({ node: profile }) => (
+                                         <a
+                                             key={profile.profileType}
+                                             href={profile.url}
+                                             target="blank"
+                                             className={`social social--${profile.profileType.toLowerCase()}`}
+                                         > </a>
+                                     ))}
+                                 </p>
+                                 <div className="sidebar__copyright">{data.datoCmsHome.copyright}</div>
+                             </div>
+                         </div>
+                         <div className="container__body">
+                             <div className="container__mobile-header">
+                                 <div className="mobile-header">
+                                     <div className="mobile-header__menu">
+                                         <Link to="#" data-js="toggleSidebar" />
+                                     </div>
+                                     <div className="mobile-header__logo">
+                                         <Link to="/">{data.datoCmsSite.globalSeo.siteName}</Link>
+                                     </div>
+                                 </div>
+                             </div>
+                             {children}
+                         </div>
+                     </div>
+                 )}
+    />
 )
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.object,
+    children: PropTypes.object,
 }
 
 export default TemplateWrapper
---
+++ src/pages/poc.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Masonry from "react-masonry-component";
+import Layout from "../components/layout";
+
+const PocPage = ({ data }) => (
+  <Layout>
+    <Masonry className="showcase">
+      {data.allQuestions.map(({
+                                id,
+                                option1,
+                                option2,
+                                option3,
+                                option4,
+                                description
+                              }) => (
+        <div key={id} className="showcase__item">
+          <figure className="card">
+            <figcaption className="card__caption">
+              <h6 className="card__title">
+                <div>{description}</div>
+              </h6>
+              <div className="card__description">
+                <ul>
+                  <li>{option1}</li>
+                  <li>{option2}</li>
+                  <li>{option3}</li>
+                  <li>{option4}</li>
+                </ul>
+              </div>
+            </figcaption>
+          </figure>
+        </div>
+      ))}
+    </Masonry>
+  </Layout>
+);
+
+export default PocPage;
+
+export const query = graphql`
+  query allQuestionsQuery {
+    allQuestions {
+      option1
+      option2
+      option3
+      option4
+      subject
+      description
+      id
+    }
+  }`;
