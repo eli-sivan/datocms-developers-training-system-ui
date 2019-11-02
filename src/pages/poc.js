e9a9097df5e10a43e89730deda09d0e4fdbea6d6
esivantemp <eli.sivan@naturalint.com>
2019-11-02 17:18:36 +0200

    support questions ui

--- src/pages/poc.js
+++ src/pages/poc.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from 'react'
 import {graphql} from "gatsby";
 import styled from 'styled-components'
 import '../styles/poc.sass'
@@ -16,29 +16,41 @@ const StyledCard = styled.div`
     padding-left: 0;
 `;
 
-const PocPage = ({data}) => (
-    <StyledPageContainer>
-        {data.allDatoCmsQuestion.edges.map(({node: question}, index) => (
-            <StyledCard key={question.id} className='showcase__item'>
-                <figure className={(index > 0 && 'hide' || '') + " card"}>
-                    <figcaption className="card__caption">
-                        <h6 className="card__title">
-                            <div>{question.description}</div>
-                        </h6>
-                        <div className="card__description">
-                            <ul>
-                                <li>{question.option1}</li>
-                                <li>{question.option2}</li>
-                                <li>{question.option3}</li>
-                                <li>{question.option4}</li>
-                            </ul>
-                        </div>
-                    </figcaption>
-                </figure>
-            </StyledCard>
-        ))}
-    </StyledPageContainer>
-);
+const PocPage = ({data}) => {
+    const [questionIndex, setIndex] = useState(0);
+    const handleAnsweredQuestion = function (clickedAnswer) {
+        const { node: question } = data.allDatoCmsQuestion.edges.find((_, index) => index === questionIndex);
+        if (question.answer == clickedAnswer) {
+            alert(true);
+        } else {
+            alert(false);
+        }
+        setIndex(questionIndex + 1);
+    }
+    return (
+        <StyledPageContainer>
+            {data.allDatoCmsQuestion.edges.map(({node: question}, index) => questionIndex === index && (
+                <StyledCard key={question.id} className='showcase__item'>
+                    <figure className="card">
+                        <figcaption className="card__caption">
+                            <h6 className="card__title">
+                                <div>{question.description}</div>
+                            </h6>
+                            <div className="card__description">
+                                <ul>
+                                    <li onClick={() => handleAnsweredQuestion(1)}>{question.option1}</li>
+                                    <li onClick={() => handleAnsweredQuestion(2)}>{question.option2}</li>
+                                    <li onClick={() => handleAnsweredQuestion(3)}>{question.option3}</li>
+                                    <li onClick={() => handleAnsweredQuestion(4)}>{question.option4}</li>
+                                </ul>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </StyledCard>
+            ))}
+        </StyledPageContainer>
+    );
+}
 
 export default PocPage;
 
