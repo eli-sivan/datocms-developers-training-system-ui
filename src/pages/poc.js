5fa97050da68051ddbde59f82e49503f7c442433
esivantemp <eli.sivan@naturalint.com>
2019-11-03 10:53:31 +0200

    remove styled

--- src/pages/poc.js
+++ src/pages/poc.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import {graphql} from "gatsby";
-import styled from 'styled-components'
+import styled from "@emotion/styled"
 import '../styles/poc.sass'
 
 const StyledPageContainer = styled.div`
